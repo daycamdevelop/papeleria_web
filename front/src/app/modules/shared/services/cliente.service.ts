2202483f42c53182981cf16e5825de5181a7789e
Carlos Rincon <cdrinconm@unal.edu.co>
2025-05-07 16:21:35 -0500

    Apuntamiento endpoint back

--- front/src/app/modules/shared/services/cliente.service.ts
+++ front/src/app/modules/shared/services/cliente.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-const base_url = "http://localhost:8081/api/v1";
+const base_url = "http://186.29.194.235:8081/api/v1";
 
 @Injectable({
   providedIn: 'root'
