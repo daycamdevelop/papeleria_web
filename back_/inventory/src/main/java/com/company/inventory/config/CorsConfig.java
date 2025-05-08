d7957882ecbcf3394b86653db34a2e19b7793c3e
Carlos Rincon <cdrinconm@unal.edu.co>
2025-05-07 21:00:33 -0500

    Ajustes enpoint de render a servidor v4

--- back_/inventory/src/main/java/com/company/inventory/config/CorsConfig.java
+++ back_/inventory/src/main/java/com/company/inventory/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Aplica a todas las rutas
-        .allowedOrigins("https://papeleria-web-vn2s.onrender.com")
+        .allowedOrigins("https://daycamdevelop.github.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
