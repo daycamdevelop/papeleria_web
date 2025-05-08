ae638a8f43ff25cd6f7d29e1e0c8ae52f4a0d353
Carlos Rincon <cdrinconm@unal.edu.co>
2025-05-07 21:09:57 -0500

    Ajustes enpoint de render a servidor v5

--- back_/inventory/src/main/java/com/company/inventory/controller/ClienteRestController.java
+++ back_/inventory/src/main/java/com/company/inventory/controller/ClienteRestController.java
@@ -8,7 +8,7 @@ import com.company.inventory.model.Cliente;
 import com.company.inventory.response.ClienteResponseRest;
 import com.company.inventory.services.interfaces.IClienteService;
 
-@CrossOrigin(origins = {"http://localhost:4200", "https://papeleria-web-vn2s.onrender.com"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://daycamdevelop.github.io"})
 @RestController
 @RequestMapping("/api/v1")
 public class ClienteRestController {
--- back_/inventory/src/main/java/com/company/inventory/controller/ProveedorRestController.java
+++ back_/inventory/src/main/java/com/company/inventory/controller/ProveedorRestController.java
@@ -17,7 +17,7 @@ import com.company.inventory.model.Proveedor;
 import com.company.inventory.response.ProveedorResponseRest;
 import com.company.inventory.services.interfaces.IProveedorService;
 
-@CrossOrigin(origins = {"https://papeleria-web-vn2s.onrender.com"})
+@CrossOrigin(origins = {"https://daycamdevelop.github.io"})
 @RestController
 @RequestMapping("/api/v1")
 public class ProveedorRestController {
